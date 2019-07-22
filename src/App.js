e9ce5060ba429cffaa6dcc2e27fdc7df85177cbb
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-07-22 14:42:33 -0400

    this is a really cool concept! Cant wait to apply it in the next challenge!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router} from "react-router-dom";
+
 import { Home, About, Contact, Navigation } from './components';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => (
-  <div>
-    <Navigation />
-  </div>
+  <Router>
+    {/* <Navigation /> */}
+    <Switch>
+      <Route path='/' exact component={Navigation} />
+      <Route path='/About' component={Navigation} />
+      <Route path='/Contact' component={Navigation} />
+    </Switch>
+  </Router>
 );
 
 export default App;
